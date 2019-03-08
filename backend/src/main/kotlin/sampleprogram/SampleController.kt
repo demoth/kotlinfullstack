09a4cad7175d677c9a90259c8b442050d9156655
demoth <bubnov.d.e@gmail.com>
2019-03-08 05:08:47 -0800

    add bootstap material design style

--- backend/src/main/kotlin/sampleprogram/SampleController.kt
+++ backend/src/main/kotlin/sampleprogram/SampleController.kt
@@ -3,17 +3,38 @@ package sampleprogram
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
 
 @RestController
 open class SampleController {
+    private val r = Random(0)
+
+    private val users = listOf(
+            "Alyce Sprow",
+            "Nathan Croslin",
+            "Stacey Jessen",
+            "Sherlyn Franson",
+            "Maryjane Ponton",
+            "Kirstin Remick",
+            "Kiley Paskett",
+            "Nickole Emert",
+            "Lynwood Penton",
+            "Tenisha Stratton",
+            "Enrique Delorme",
+            "Charolette Wuest",
+            "Della Fishburn",
+            "Marylou Leth",
+            "Michele Prather",
+            "Raquel Jen",
+            "Catheryn Cooley",
+            "Mitchel Gremillion",
+            "Augustina Belli")
+
 
     private val json = ObjectMapper()
 
     @GetMapping("/sample")
     fun getSample(): String {
-        return json.writeValueAsString(arrayListOf(
-                User("Alpha", 10),
-                User("Omega", 20)
-        ))
+        return json.writeValueAsString(users.filter { r.nextBoolean() }.map { User(it, r.nextInt(18, 80)) })
     }
 }
--- frontend/src/main/kotlin/sampleprogram/SamplePage.kt
+++ frontend/src/main/kotlin/sampleprogram/SamplePage.kt
@@ -1,32 +1,65 @@
 package sampleprogram
 
+import kotlinx.html.ButtonType
 import kotlinx.html.dom.append
+import kotlinx.html.js.button
+import kotlinx.html.js.div
+import kotlinx.html.js.onClickFunction
 import kotlinx.html.js.p
-import org.w3c.dom.HTMLElement
-import org.w3c.dom.HTMLTextAreaElement
+import kotlinx.html.span
 import org.w3c.dom.events.Event
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
+import kotlin.dom.clear
 
 fun samplePageInit() {
-    ajaxget("/sample") {
-        println(it)
-        val users = JSON.parse<Array<User>>(it)
 
-        val textarea = document.getElementById("textarea_id") as HTMLTextAreaElement
-        textarea.value = ""
-        users.forEach { u ->
-            textarea.value += "Name: ${u.name}, Age: ${u.age}\n"
+    // create header
+    document.getElementById("header")?.append {
+        button(classes = "btn btn-primary", type = ButtonType.button) {
+            +"Home"
         }
-        val root = document.getElementById("root") as HTMLElement
-
-        root.append {
-            p {
-                +"Sample status: Ok"
-            }
+        button(classes = "btn btn-secondary", type = ButtonType.button) {
+            +"Screen1"
+        }
+        button(classes = "btn btn-secondary", type = ButtonType.button) {
+            +"About"
         }
     }
 
+    document.getElementById("content")?.append {
+        div("card-body") {
+            p("card-text") {
+                +"""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"""
+            }
+            p("card-text") {
+                +"This is example of a fullstack kotlin application."
+            }
+            button(classes = "btn btn-primary active", type = ButtonType.button) {
+                +"Get list of users"
+                onClickFunction = {
+                    ajaxget("/sample") {
+                        val users = JSON.parse<Array<User>>(it)
+                        val userlist = document.getElementById("userlist") ?: return@ajaxget {}
+                        userlist.clear()
+                        userlist.append {
+                            users.forEach { u ->
+                                div("alert alert-secondary") {
+                                    +u.name
+                                    span { +" " }
+                                    span("badge badge-primary") { +"${u.age}" }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 fun ajaxget(url: String, onloadend: (String) -> dynamic) {
@@ -34,6 +67,4 @@ fun ajaxget(url: String, onloadend: (String) -> dynamic) {
     req.onloadend = fun(event: Event) { onloadend(req.responseText) }
     req.open("GET", url, true)
     req.send()
-
-    println("Done")
 }
